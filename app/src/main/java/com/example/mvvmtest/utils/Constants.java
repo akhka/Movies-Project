c5ddc4a5cbd58fe7ccc8e85f6ea2471f859759db
akhka <55639743+akhka@users.noreply.github.com>
2019-10-07 01:27:14 +0300

    updated

--- app/src/main/java/com/example/mvvmtest/utils/Constants.java
+++ app/src/main/java/com/example/mvvmtest/utils/Constants.java
@@ -2,7 +2,9 @@ package com.example.mvvmtest.utils;
 
 public class Constants {
 
-    public final static String TMDB_API_KEY = "f0c41bfab32e883b3a71d1005a970316";
+
+    //TODO: Put your API key here
+    public final static String TMDB_API_KEY = "PUT YOUR API KEY HERE";
 
     public final static String BASE_URL = "https://api.themoviedb.org/3/";
     public final static String IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500";
